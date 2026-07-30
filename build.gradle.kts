7e4c525a9365a628e2f89edd9d013504242885c8
Jsinco <jonah@jsinco.dev>
2026-07-30 19:52:00 -0400

    Update Wyck (BiomesAPI)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     implementation("eu.okaeri:okaeri-configs-yaml-bukkit:5.0.5")
-    implementation("dev.wyck:Wyck:3.2.0")
+    implementation("dev.wyck:wyck:3.3.0-4e557c5")
 
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9-beta1")
 
@@ -44,7 +44,7 @@ tasks {
     shadowJar {
         val shaded = "dev.lumas.biomes.shaded"
         relocate("eu.okaeri", "$shaded.okaeri")
-        relocate("dev.wyck", "$shaded.biomesapi")
+        relocate("dev.wyck", "$shaded.wyck")
         archiveClassifier.set("")
     }
 
