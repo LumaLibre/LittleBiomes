ef6a935b50489c585d27b2cc43bcb40bdce21a49
Jsinco <jonah@jsinco.dev>
2026-09-09 18:31:06 -0400

    Update Wyck

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     implementation("eu.okaeri:okaeri-configs-yaml-bukkit:5.0.5")
-    implementation("dev.wyck:wyck:3.3.0")
+    implementation("dev.wyck:wyck-vanilla:3.4.0-2da412f")
 
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9-beta1")
 
